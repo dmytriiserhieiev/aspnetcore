d85efc20aa31af630d5f1239e8d6c163c68d67a5
James Newton-King <james@newtonking.com>
2022-09-22 20:47:45 +0800

    Add header dictionary analyzer and fixer

---
+++ src/Framework/AspNetCoreAnalyzers/test/Http/HeaderDictionaryIndexerAnalyzerTests.cs
@@ -0,0 +1,118 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.CodeAnalysis.Testing;
+using VerifyCS = Microsoft.AspNetCore.Analyzers.Verifiers.CSharpAnalyzerVerifier<
+    Microsoft.AspNetCore.Analyzers.Http.HeaderDictionaryIndexerAnalyzer>;
+
+namespace Microsoft.AspNetCore.Analyzers.Http;
+
+public class HeaderDictionaryIndexerAnalyzerTests
+{
+    [Fact]
+    public async Task IHeaderDictionary_Get_MismatchCase_ReturnDiagnostic()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+var webApp = WebApplication.Create();
+webApp.Use(async (HttpContext context, Func<Task> next) =>
+{
+    var s = {|#0:context.Request.Headers[""content-type""]|};
+    await next();
+});
+",
+        new DiagnosticResult(DiagnosticDescriptors.UseHeaderDictionaryPropertiesInsteadOfIndexer)
+            .WithLocation(0)
+            .WithMessage("The header 'content-type' can be accessed using the ContentType property"));
+    }
+
+    [Fact]
+    public async Task IHeaderDictionary_Set_MismatchCase_ReturnDiagnostic()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+var webApp = WebApplication.Create();
+webApp.Use(async (HttpContext context, Func<Task> next) =>
+{
+    {|#0:context.Request.Headers[""content-type""]|} = """";
+    await next();
+});
+",
+        new DiagnosticResult(DiagnosticDescriptors.UseHeaderDictionaryPropertiesInsteadOfIndexer)
+            .WithLocation(0)
+            .WithMessage("The header 'content-type' can be accessed using the ContentType property"));
+    }
+
+    [Fact]
+    public async Task IHeaderDictionary_Get_UnknownProperty_NoDiagnostic()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+var webApp = WebApplication.Create();
+webApp.Use(async (HttpContext context, Func<Task> next) =>
+{
+    context.Request.Headers[""content-type1""] = """";
+    await next();
+});
+");
+    }
+
+    [Fact]
+    public async Task IHeaderDictionary_Get_VariableProperty_NoDiagnostic()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+var webApp = WebApplication.Create();
+webApp.Use(async (HttpContext context, Func<Task> next) =>
+{
+    var s = """";
+    context.Request.Headers[s] = """";
+    await next();
+});
+");
+    }
+
+    [Fact]
+    public async Task HeaderDictionary_Get_KnownProperty_NoDiagnostic()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using Microsoft.AspNetCore.Http;
+var headers = new HeaderDictionary();
+headers[""Content-Type""] = """";
+");
+    }
+
+    [Fact]
+    public async Task HeaderDictionary_CastToIHeaderDictionary_Get_KnownProperty_ReturnDiagnostic()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyAnalyzerAsync(@"
+using Microsoft.AspNetCore.Http;
+IHeaderDictionary headers = new HeaderDictionary();
+{|#0:headers[""Content-Type""]|} = """";
+",
+        new DiagnosticResult(DiagnosticDescriptors.UseHeaderDictionaryPropertiesInsteadOfIndexer)
+            .WithLocation(0)
+            .WithMessage("The header 'Content-Type' can be accessed using the ContentType property"));
+    }
+}
